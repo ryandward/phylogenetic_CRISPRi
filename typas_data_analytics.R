d323b021a9bf39a2e153cc37202ba28d784cc17e
Ryan Valverde de Ward <>
2022-02-19 15:25:01 -0600

    renamed variables, removed amiguous genes

--- typas_data_analytics.R
+++ typas_data_analytics.R
@@ -1,6 +1,6 @@
 library(pacman)
 
-p_load(data.table)
+p_load(data.table, pheatmap)
 
 #https://www.cell.com/fulltext/S0092-8674(10)01374-7
 typas_data <- fread("mmc2.tsv")
@@ -10,13 +10,13 @@ typas_data <- melt(typas_data,
                    value = "score", 
                    id.vars = "Gene")
 
-typas_data[, c("ECK_name", paste0(rep("trash",5), c(1:5))) := tstrsplit(Gene, "-", type.convert = TRUE, fixed = TRUE)]
+typas_data[, c("ECK_name", paste0(rep("extra",5), c(1:5))) := tstrsplit(Gene, "-", type.convert = TRUE, fixed = TRUE)]
 
-typas_conversion <- melt(typas_data[, .(ECK_name, trash1, trash2, trash3, trash4, trash5)], id.vars = "ECK_name", variable.name = "trash", value.name = "synonym")[!is.na(synonym)]
+typas_conversion <- melt(typas_data[, .(ECK_name, extra1, extra2, extra3, extra4, extra5)], id.vars = "ECK_name", variable.name = "extra", value.name = "synonym")[!is.na(synonym)]
 
-typas_conversion[, trash := NULL]
+typas_conversion[, extra := NULL]
 
-typas_data[, paste0(rep("trash",5), c(1:5)) := NULL]
+typas_data[, paste0(rep("extra",5), c(1:5)) := NULL]
 
 typas_data[, Gene := NULL]
 
@@ -43,3 +43,38 @@ conversion[, name_type := NULL]
 conversion <- unique(conversion)
 
 conversion_clues <- unique(typas_conversion[typas_data[!ECK_name %in% conversion$ECK_name, .(ECK_name = unique(ECK_name))], on = .(ECK_name)])
+
+conversion_clues[!is.na(synonym), synonym := paste0(tolower(substr(synonym, 1, 3)), substr(synonym, 4, length(synonym)))]
+
+conversion_clues[
+  synonym %in% conversion$long_name, 
+  b_name := conversion[long_name %in% conversion_clues$synonym][
+    conversion_clues[synonym %in% conversion$long_name], 
+    on = .(long_name == synonym)]$b_name]
+
+conversions_found <- conversion_clues[!is.na(b_name)]
+
+setnames(conversions_found, old = "synonym", new = "long_name")
+
+conversion <- rbind(conversion, conversions_found)
+
+typas_data <- conversion[typas_data, on = .(ECK_name)][!is.na(b_name)]
+
+# get rid of non-unique b names
+typas_data <- 
+  typas_data[
+    !b_name %in% typas_data[, .N, by = .(b_name)][N != nrow(typas_data[, .(unique(condition))])]$b_name]
+
+# get rid of non-unique ECK names
+typas_data <- 
+  typas_data[
+    !b_name %in% typas_data[, .N, by = .(ECK_name)][N != nrow(typas_data[, .(unique(condition))])]$b_name]
+
+typas_data <- dcast(typas_data, b_name ~ condition, value.var = "score")
+
+typas_data_matrix <- data.matrix(typas_data[, -1])
+
+rownames(typas_data_matrix) <- typas_data$b_name
+
+typas_data_matrix <- typas_data_matrix[complete.cases(typas_data_matrix),]
+
