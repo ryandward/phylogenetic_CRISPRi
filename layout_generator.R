ee3b3f881bf157f4c67c16e841c82e439fa503b5
Ryan Ward <ryandward@gmail.com>
2022-03-31 16:32:45 -0500

    new data

--- layout_generator.R
+++ layout_generator.R
@@ -16,3 +16,12 @@ Ryan_Strains <- data.table(dbReadTable(chem_gen_db, "Ryan_Strains"))
 Well_Stats <- data.table(dbReadTable(chem_gen_db, "Well_Stats"))
 
 dbDisconnect(chem_gen_db)
+
+Well_Stats[, Row := gsub("[0-9]{1,2}", "", Well)]
+
+Well_Stats[, Column := as.numeric(gsub("[A-Z]", "", Well))]
+
+setorder(Well_Stats, Column, Row)
+
+Well_Stats[, Summary := paste(Media, Rep, Induced, Organism, Chemical, Dose)]
+
