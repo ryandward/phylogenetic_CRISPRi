0831cb0003e8837f2478abbe52b863fe847fbebe
Ryan Valverde de Ward <ryandward@gmail.com>
2023-12-01 18:10:32 -0600

    fix: initialization error

--- attempt2/2022 11 10 Imipenem.R
+++ attempt2/2022 11 10 Imipenem.R
@@ -8,9 +8,6 @@ data[Row %in% c("B", "C", "D"), Induced := FALSE]
 
 data[Row %in% c("E", "F", "G"), Induced := TRUE]
 
-# Initialize Imipenem
-data[, Imipenem := NULL]
-
 # Set Imipenem for Columns 11 to 3
 for (col in 11:3) {
   data[Column == as.character(col), Imipenem := 32 / 2^(11 - col)]
@@ -28,7 +25,6 @@ data[, Seconds := as.numeric(gsub("s", "", Seconds))]
 ggplot(data, aes(x = Seconds, y = log10(OD600), group = interaction(Imipenem, Induced))) +
   stat_summary(fun = mean, geom = "line", aes(color = factor(Imipenem), linetype = factor(Induced))) +
   stat_summary(fun.data = mean_se, geom = "ribbon", aes(fill = factor(Imipenem)), alpha = 0.2) +
-  labs(title = "E. coli library growth", x = "Seconds", y = "Growth (log OD600)",
-       color = "Imipenem", linetype = "Induced", fill = "Imipenem") +
+  labs(title = "E. coli library growth", x = "Seconds", y = "Growth (log OD600)", color = "Imipenem", linetype = "Induced", fill = "Imipenem") +
   scale_linetype_manual(values = c("FALSE" = "dashed", "TRUE" = "solid")) +
   theme_minimal()
