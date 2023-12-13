9500e7ca992ce56b49c7872af846ab50c066d4a8
Ryan Ward <ryandward@gmail.com>
2023-12-13 14:53:51 -0600

    filtered quality guides

--- Sequencing/M005389/ECimiRDW1-12.r
+++ Sequencing/M005389/ECimiRDW1-12.r
@@ -221,5 +221,6 @@ definitions <- fread("Organisms/E_coli_genes_from_string.tsv",
   mutate(locus_tag = gsub(".*\\.", "", string_id))
 
 results_summary <- results %>%
+  filter(type == "perfect") %>%
   dcast(locus_tag ~ contrast, value.var = "logFC", fun.aggregate = median) %>%
   left_join(definitions)
