1d45a541000d2e325a7b610a89eb32614d6cdb59
Ryan David Ward <ryandward@gmail.com>
2024-10-25 16:56:31 -0500

    Refactor ECO_proQ_delta_biolog_11C.R by removing unused functions and cleaning up the growth curve plotting code

--- Followups/proQ/Growth_curves/ECO_proQ_delta_biolog_11C.R
+++ Followups/proQ/Growth_curves/ECO_proQ_delta_biolog_11C.R
@@ -2,10 +2,6 @@ library(tidyverse) # Load tidyverse for data manipulation and visualization
 library(data.table) # Load data.table for fast and efficient data manipulation
 library(ggtext) # Load markdown for formatting text (e.g., italicizing species names)
 
-# ============================
-# Functions Section
-# ============================
-
 # Read the data file and perform some initial data transformations
 data <- fread("Followups/proQ/Growth_curves/ECO_proQ_delta_biolog_11C_20241125.tsv") %>%
   select(-`Cycle Nr.`, -`Temp. [°C]`) %>%
@@ -41,32 +37,4 @@ growth_curve_plot <- data %>%
   ) +
   ggtitle("Growth Curves of *E. coli* WT in Biolog 11C Plates")
 
-print(growth_curve_plot)
-
-
-# Add ribbons to indicate standard error around the mean
-stat_summary(fun.data = mean_se, geom = "ribbon", aes(fill = factor(Imipenem)), alpha = 0.4) +
-  # Add labels to the plot
-  labs(
-    title = "*E. coli* growth in WT and Δ*proQ* strains with varying Imipenem concentrations",
-    x = "Hours",
-    y = "Growth (log OD600)",
-    color = "Imipenem",
-    linetype = "Strain",
-    fill = "Imipenem"
-  ) +
-  # Customize line types for the different strains
-  scale_linetype_manual(values = c("Δ*proQ*" = "dashed", "WT" = "solid")) +
-  # Facet by strain to separate the plots for better visualization
-  facet_grid(~Strain) +
-  theme_minimal() + # Use a minimal theme for a clean look
-  theme(
-    plot.title = element_markdown(), # Enable markdown in the plot title
-    axis.title.x = element_markdown(), # Enable markdown in the x-axis title
-    axis.title.y = element_markdown(), # Enable markdown in the y-axis title
-    strip.text = element_markdown(), # Enable markdown in the facet labels
-    legend.text = element_markdown() # Enable markdown in the legend labels
-  )
-
-# Print the growth curve plot
 print(growth_curve_plot)
